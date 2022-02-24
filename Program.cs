6bf066e1b7eb4814dda3d25457a95add3e41967e
watsiq <64712597+watsiq@users.noreply.github.com>
2022-02-24 23:14:21 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			RMQ rmq = new RMQ();
+			Console.WriteLine("Tekan tombol apapun untuk inisialisasi RMQ parameters.");
+			Console.ReadKey();
+			rmq.InitRMQConnection(); // inisialisasi parameter (secara default) untuk koneksi ke server RMQ
+			Console.WriteLine("Tekan tombol apapun untuk membuka koneksi ke RMQ.");
+			Console.ReadKey();
+			rmq.CreateRMQConnection(); // memulai koneksi dengan RMQ
+			Console.Write("Masukkan nama queue channel untuk menerima pesan melalui RMQ.\n>> ");
+			string queue_name = Console.ReadLine();
+			Console.WriteLine("Menunggu pesan masuk...");
+			rmq.WaitingMessage(queue_name);
+		}
+    }
+}
---
+++ RMQ.cs
@@ -0,0 +1,70 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receiver
+{
+    class RMQ
+    {
+        public ConnectionFactory connectionFactory;
+        public IConnection connection;
+        public IModel channel;
+        private bool isReceiving = false;
+        public void InitRMQConnection(string host = "cloudrmqserver.pptik.id", int port = 5672, string user = "tmdgdai",
+         string pass = "tmdgdai", string vhost = "/tmdgdai")
+        {
+            connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = host;
+            //connectionFactory.Port = port;
+            connectionFactory.UserName = user;
+            connectionFactory.Password = pass;
+            connectionFactory.VirtualHost = vhost;
+        }
+        public void CreateRMQConnection()
+        {
+            connection = connectionFactory.CreateConnection();
+            Console.WriteLine("Koneksi " + (connection.IsOpen ? "Berhasil!" : "Gagal!"));
+        }
+        public void WaitingMessage(string exchangeName = "logs")
+        {
+            using (channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: exchangeName, type: "fanout");
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: "");
+                Console.WriteLine(" [*] Waiting for logs.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Pesan diterima: {0}", message);
+                };
+                channel.BasicConsume(queue: queueName,
+                autoAck: true,
+                consumer: consumer);
+
+                Console.WriteLine(" Tekan [enter] untuk memutus koneksi.");
+                Console.ReadLine();
+                Disconnect();
+            }
+        }
+        public void Disconnect()
+        {
+            isReceiving = false;
+            channel.Close();
+            channel = null;
+            Console.WriteLine("Channel ditutup!");
+            if (connection.IsOpen)
+            {
+                connection.Close();
+            }
+            Console.WriteLine("Koneksi diputus!");
+            connection.Dispose();
+            connection = null;
+        }
+    }
+}
